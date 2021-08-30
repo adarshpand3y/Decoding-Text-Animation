41c598e0db81512bed66c78c0aa2250c5ff6c1b6
Adarsh Pandey <mail.apandey2001@gmail.com>
2021-08-30 18:04:16 +0530

    updated style and created createurl.js

---
+++ js/craeteurl.js
@@ -0,0 +1,35 @@
+window.onload = (event) => {
+    var text = "";
+    function makeid(length, t) {
+        var result = '';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for (var i = 0; i < length; i++) {
+            if (t.charAt(i) == ' ') {
+                result += " ";
+            }
+            else {
+                result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            }
+        }
+        return result;
+    }
+
+    text = "Create your custom message";
+    console.log("Text: ", text);
+    text = text.replaceAll("+", " ");
+    r = document.getElementById("reveal");
+    l = text.length;
+    randomText = makeid(l, text);
+    r.innerHTML = "";
+    let char = 0;
+    let interval = setInterval(function () {
+        actual = text.substring(0, char + 1);
+        ran = makeid(randomText.length - char - 1, text.substring(char + 1));
+        r.innerHTML = actual + ran;
+        char++;
+        if (char === text.length) {
+            clearInterval(interval);
+        }
+    }, 50);
+}
