089d3dcccb550d579cf05ef098de8336e1ddd177
Adarsh Pandey <mail.apandey2001@gmail.com>
2021-08-30 17:08:09 +0530

    improved random string generation and display logic, created createurl.html

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 window.onload = (event) => {
     var text = "";
-    function makeid(length) {
+    function makeid(length, t) {
         var result = '';
-        var characters = 'ABCDE FGHIJ KLMNO PQRST UVWXYZ abcde fghij klmno pqrst uvwxyz 01234 56789 ';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
         var charactersLength = characters.length;
         for (var i = 0; i < length; i++) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            if (t.charAt(i) == ' ') {
+                result += " ";
+            }
+            else {
+                result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            }
         }
         return result;
     }
@@ -13,21 +18,32 @@ window.onload = (event) => {
     const queryString = window.location.search;
     if (queryString == "") {
         text = "Decoding Text Animation";
+        console.log("No Querystring");
     }
     else {
         const urlParams = new URLSearchParams(queryString);
         const message = urlParams.get('message');
-        text = message;
+        if (message == null) {
+            text = "No message parameter found. Please recheck your URL.";
+        }
+        else {
+            text = message;
+            // Reversing the string
+            text = text.split("").reverse().join("");
+        }
+        console.log("Querystring found");
     }
 
+    console.log("Text: ", text);
+    text = text.replaceAll("+", " ");
     r = document.getElementById("reveal");
     l = text.length;
-    randomText = makeid(l);
+    randomText = makeid(l, text);
     r.innerHTML = "";
     let char = 0;
     let interval = setInterval(function () {
         actual = text.substring(0, char + 1);
-        ran = makeid(randomText.length - char - 1);
+        ran = makeid(randomText.length - char - 1, text.substring(char + 1));
         r.innerHTML = actual + ran;
         char++;
         if (char === text.length) {
